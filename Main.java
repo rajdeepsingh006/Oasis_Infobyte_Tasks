7afcc4fbfe568f78d273698a784c8e8a75c2ed21
rajdeepsingh006 <147084619+rajdeepsingh006@users.noreply.github.com>
2024-08-26 20:42:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int min = 1;
+        int max = 100;
+        int numberToGuess = random.nextInt(max - min + 1) + min; // Random number between min and max
+        int numberOfTries = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I have selected a number between " + min + " and " + max + ". Try to guess it!");
+
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int userGuess = scanner.nextInt();
+            numberOfTries++;
+
+            if (userGuess < min || userGuess > max) {
+                System.out.println("Please enter a number between " + min + " and " + max + ".");
+            } else if (userGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You've guessed the number in " + numberOfTries + " tries.");
+            }
+        }
+
+        scanner.close();
+    }
+}
